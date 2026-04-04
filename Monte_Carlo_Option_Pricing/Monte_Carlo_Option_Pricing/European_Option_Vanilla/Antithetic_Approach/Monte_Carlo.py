343530994c568c5d228d91cae9a9144d33a3981f
1-xxxx <1.jingxiliu@gmail.com>
2026-04-03 23:58:02 -0500

    Add files via upload

---
+++ Monte_Carlo_Option_Pricing/Monte_Carlo_Option_Pricing/European_Option_Vanilla/Antithetic_Approach/Monte_Carlo.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+# S0 : initial stock price
+# K  : strike price
+# r  : risk-free rate (continuous compounding)
+# q  : continuous dividend yield
+# sigma : volatility
+# T  : time to maturity
+# ST : price at maturity
+
+# Zi ​∼ N(0,1), i=1,2,…,N
+# generate N random variable for simulation
+def get_Z(N):
+    Z = np.random.randn(N)
+    return Z
+
+# S_T = S_0 * exp((r - q - 0.5*sigma^2)*T + sigma*sqrt(T)*Z)
+# terminal stock price under risk-neutral measure
+def get_ST(S0, r, q, sigma, T, Z):
+    ST = S0 * np.exp((r - q - 0.5 * (sigma**2)) * T + sigma * np.sqrt(T) * Z)
+    return ST
+
+# put payoff = max(K - S_T, 0), call is the opposite
+def get_put_payoff(K, ST):
+    payoff = np.maximum(K - ST, 0)
+    return payoff
+
+def get_call_payoff(K, ST):
+    payoff = np.maximum(ST - K, 0)
+    return payoff
+
+# discount the payoff to current value, and then calculate the average of them
+def get_put_estimate(S0, K, r, q, sigma, T, N):
+    Z = get_Z(N)
+    ST = get_ST(S0, r, q, sigma, T, Z)
+    payoff = get_put_payoff(K, ST)
+    discounted_payoff = np.exp(-r * T) * payoff
+    estimate_price = (1 / N) * np.sum(discounted_payoff)
+    return estimate_price
+
+# a put together function only for closed form benchmark use
+def get_estimate_discounted_payoff(S0, K, r, q, sigma, T, N):
+    Z = get_Z(N)
+    ST = get_ST(S0, r, q, sigma, T, Z)
+    payoff = get_put_payoff(K, ST)
+    discounted_payoff = np.exp(-r * T) * payoff
+    estimate_price = (1 / N) * np.sum(discounted_payoff)
+    return estimate_price, discounted_payoff
+
+
+
+
---
+++ Monte_Carlo_Option_Pricing/Monte_Carlo_Option_Pricing/European_Option_Vanilla/Antithetic_Approach/Pricing.py
@@ -0,0 +1,77 @@
+import numpy as np
+from scipy.stats import norm
+from Monte_Carlo_European import get_estimate_discounted_payoff
+
+# S0 : initial stock price
+# K  : strike price
+# r  : risk-free rate (continuous compounding)
+# q  : continuous dividend yield
+# sigma : volatility
+# T  : time to maturity
+# ST : price at maturity
+
+def get_SE_CI(S0, K, r, q, sigma, T, N):
+    estimate_price, discounted_payoff = get_estimate_discounted_payoff(S0, K, r, q, sigma, T, N)
+    SE = np.sqrt(1 / (N - 1) * np.sum((discounted_payoff - estimate_price)**2)) / np.sqrt(N)
+    CI_low, CI_high = estimate_price - SE * 1.96, estimate_price + SE * 1.96
+    return SE, CI_low, CI_high
+
+def bsm_option_value(S0, K, r, q, sigma, T, call = True):
+    d1 = (np.log(S0/K) + (r - q + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    C = S0 * np.exp(-q * T) * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+    P = K * np.exp(-r * T) * norm.cdf(-d2) - S0 * np.exp(-q * T) * norm.cdf(-d1)
+
+    if (call):
+        return C
+    return P
+
+# We are using the variance reduction technique, antithetic approach,
+# which generates N/2 samples z from N~(0,1) and generates N/2 samples -z. 
+# We take the average of two paths to cancel out extreme outliers.
+
+def get_SE_CI_antithetic(S0, K, r, q, sigma, T, N):
+
+    z = np.random.standard_normal(N)
+
+    drift = (r - q - 0.5 * sigma**2) * T
+    vol_sqrt_t = sigma * np.sqrt(T)
+    discount = np.exp(-r * T)
+    
+    ST1 = S0 * np.exp(drift + vol_sqrt_t * z)
+    ST2 = S0 * np.exp(drift + vol_sqrt_t * (-z))
+
+    payoff1 = np.maximum(K - ST1, 0) * discount
+    payoff2 = np.maximum(K - ST2, 0) * discount
+
+    antithetic_averages = (payoff1 + payoff2) / 2.0
+    
+
+    estimate_price = np.mean(antithetic_averages)
+  
+    SE = np.std(antithetic_averages, ddof=1) / np.sqrt(N)
+    
+    CI_low = estimate_price - 1.96 * SE
+    CI_high = estimate_price + 1.96 * SE
+    
+    return SE, CI_low, CI_high
+
+
+
+S0 = 100
+K = 100
+T = 0.5
+r = 0.04
+q = 0.02
+sigma = 0.2
+
+N = 10 # trials
+
+results = get_SE_CI(S0, K, r, q, sigma, T, N)
+print(f"Standard Error: {results[0]:.2f}")
+print(f"95% Confidence Interval: [{results[1]:.2f}, {results[2]:.2f}]")
+
+antithetic = get_SE_CI_antithetic(S0, K, r, q, sigma, T, N)
+print(f"Standard Error, {antithetic[0]:.2f}")
+print(f"95% Confidence Interval with antithetic approach: [{antithetic[1]:.2f}, {results[2]:.2f}]")
